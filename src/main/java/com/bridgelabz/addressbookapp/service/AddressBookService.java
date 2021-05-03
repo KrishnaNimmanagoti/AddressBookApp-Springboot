f1195c31f359fda600ba9709a9b81d22b5eb516a
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-05-03 06:59:56 +0530

    Added Lombok lib and for logging @slf4j, Database Settiing and data validation and Exception Handling

--- src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
@@ -10,45 +10,83 @@ import com.bridgelabz.addressbookapp.dto.ContactDTO;
 import com.bridgelabz.addressbookapp.model.Contact;
 import com.bridgelabz.addressbookapp.repo.IAddressBookRepo;
 
+/**
+ * connection with server
+ * 
+ * @author Krishna
+ *
+ */
 @Service
 public class AddressBookService implements IAddressBookService {
 
 	@Autowired
 	private IAddressBookRepo addressBookRepo;
-	
+
+	/**
+	 * method add new Contact
+	 * 
+	 * @param contact
+	 * @return Contact
+	 */
 	@Override
 	public Contact addContact(ContactDTO contactDTO) {
 		Contact contact = new Contact(contactDTO);
 		return this.addressBookRepo.save(contact);
 	}
 
+	/**
+	 * method to get list of Contacts
+	 * 
+	 * @return list of Contacts
+	 */
 	@Override
 	public List<Contact> getContacts() {
 		return addressBookRepo.findAll();
 	}
-	
+
+	/**
+	 * method to get Contact by id
+	 * 
+	 * @param personId
+	 * @return Contact Data
+	 */
 	@Override
-	public Contact updateContact(ContactDTO contactDTO, Integer id) {
-		
-		return addressBookRepo.findById(id)
-		.map(contact -> {
-			contact.setFirstName(contactDTO.getFirstName());
-			contact.setLastName(contactDTO.getLastName());
+	public Optional<Contact> getContactById(Integer personId) {
+		return addressBookRepo.findById(personId);
+	}
+
+	/**
+	 * method to update a contact by id
+	 * 
+	 * @param personId
+	 * @param Contact
+	 * @return Contact details
+	 */
+	@Override
+	public Contact updateContact(ContactDTO contactDTO, Integer personId) {
+
+		return addressBookRepo.findById(personId).map(contact -> {
+			contact.setName(contactDTO.getName());
+			contact.setPhone(contactDTO.getPhone());
+			contact.setEmail(contactDTO.getEmail());
 			contact.setAddress(contactDTO.getAddress());
 			return this.addressBookRepo.save(contact);
-		})
-		.orElseGet(() -> {
+		}).orElseGet(() -> {
 			return this.addContact(contactDTO);
 		});
-		
+
 	}
 
+	/**
+	 * method to delete a contact by id
+	 * 
+	 * @param personId
+	 * @param deleted  contact data
+	 */
 	@Override
-	public Optional<Contact> deleteContact(Integer id) {
-		Optional<Contact> contact = addressBookRepo.findById(id);
-		addressBookRepo.deleteById(id);
+	public Optional<Contact> deleteContact(Integer personId) {
+		Optional<Contact> contact = addressBookRepo.findById(personId);
+		addressBookRepo.deleteById(personId);
 		return contact;
 	}
-	
-
 }
--- src/main/java/com/bridgelabz/addressbookapp/service/IAddressBookService.java
+++ src/main/java/com/bridgelabz/addressbookapp/service/IAddressBookService.java
@@ -7,13 +7,15 @@ import com.bridgelabz.addressbookapp.dto.ContactDTO;
 import com.bridgelabz.addressbookapp.model.Contact;
 
 public interface IAddressBookService {
-	
+
 	Contact addContact(ContactDTO contactDTO);
-	
+
 	List<Contact> getContacts();
-	
+
 	Contact updateContact(ContactDTO contactDTO, Integer id);
 
-	Optional<Contact> deleteContact(Integer id);
+	Optional<Contact> getContactById(Integer id);
+
+	Optional<Contact> deleteContact(Integer personId);
 
 }
